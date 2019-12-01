6ac1d1f232952e06fbd172101c7d49fd4c3e2a64
yaoweixin <1107442961@qq.com>
2019-12-01 21:44:35 +0800

    login page

---
+++ src/views/logincomponents/logo.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="container">
+    <div class="logo">
+       静
+    </div>
+    <div class="title">仪静二手交易平台</div>
+</div>
+</template>
+<script>
+export default {
+    name: 'logo'
+}
+</script>
+<style lang="stylus" scoped>
+  .logo
+    background: url('~@/assets/img/logo.png') center center/5rem 1.213333rem
+    height: 1.213333rem
+    text-align: center
+    font-size: .186667rem
+    color: #c55758
+    line-height: 1.213333rem
+    margin-top: 1.333333rem
+  .title
+    font-size: .4rem
+    color: #f58686
+    text-align: center
+    height: .426667rem
+    line-height: .426667rem
+    margin-top: .266667rem
+    font-weight: bolder
+</style>
